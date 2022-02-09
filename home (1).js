1974263b052f0728402e925086beca2583cae46f
filipe099 <98980531+filipe099@users.noreply.github.com>
2022-02-09 07:07:11 -0300

    Add files via upload

---
+++ home (1).js
@@ -0,0 +1,51 @@
+function paddedFormat(num) {
+    return num < 10 ? "0" + num : num; 
+}
+
+function startCountDown(duration, element) {
+
+    let secondsRemaining = duration;
+    let min = 0;
+    let sec = 0;
+
+    let countInterval = setInterval(function () {
+
+        min = parseInt(secondsRemaining / 60);
+        sec = parseInt(secondsRemaining % 60);
+
+        element.textContent = `${paddedFormat(min)}:${paddedFormat(sec)}`;
+
+        secondsRemaining = secondsRemaining - 1;
+        if (secondsRemaining < 0) { clearInterval(countInterval) };
+
+    }, 1000);
+}
+
+window.onload = function () {
+    let time_minutes = 0;
+    let time_seconds = 30; 
+
+    let duration = time_minutes * 60 + time_seconds;
+
+    element = document.querySelector('#count-down-timer');
+    element.textContent = `${paddedFormat(time_minutes)}:${paddedFormat(time_seconds)}`;
+
+    startCountDown(--duration, element);
+    
+};
+
+var a = document.getElementById(`a`)
+var b = document.getElementById(`b`)
+
+function contar(){
+  a.innerHTML+= 1 
+  if(a.innerHTML = "111"){
+    alert("oi")
+  }
+  
+}
+
+function contar1(){
+  b.innerHTML+= 1
+}
+
